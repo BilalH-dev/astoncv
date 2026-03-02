93f44824f1d0503153c902701ffba06fef4ca032
Bilal Haq <104397354+BilalH-dev@users.noreply.github.com>
2026-03-02 11:58:16 +0000

    P1: list CVs on index page

--- index.php
+++ index.php
@@ -1,5 +1,9 @@
 <?php
 require 'includes/db.php';
+
+function escapedString($string) {
+    return htmlspecialchars($string, ENT_QUOTES, 'UTF-8');
+}
 ?>
 
 <!DOCTYPE html>
@@ -11,6 +15,32 @@ require 'includes/db.php';
 </head>
 <body>
     <h1>AstonCV</h1>
-    <p>Setup OK</p>
+    <section id="cv-list">
+        <h2>CV List</h2>
+        <?php
+        $sql = "SELECT id, name, email, keyprogramming FROM cvs";
+        $result = $db->query($sql);
+        if ($result->rowCount() > 0) {
+            echo "<table>
+                    <tr>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Key Programming Language</th>
+                    </tr>";
+            while($row = $result->fetch()) {
+                echo "<tr>";
+                echo "<td><a href=view.php?id=". escapedString($row['id']) . ">" . escapedString($row['name']) . "</a></td>";
+                echo "<td>" . escapedString($row['email']) . "</td>";
+                echo "<td>" . escapedString($row['keyprogramming']) . "</td>";
+                echo "</tr>";
+            }
+            echo "</table>";
+            unset($result);
+        }
+        else {
+            echo "No records found.";
+        }
+        ?>
+    </section>
 </body>
 </html>
